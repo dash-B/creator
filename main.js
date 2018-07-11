5256fd3de1b5e4be56433dff042a417d733ee95a
spiderfencer <jinjoo@gmail.com>
2018-07-11 08:06:24 -0700

    added fonts

--- main.js
+++ main.js
@@ -1,41 +1,76 @@
 window.onload = () => {
+ const allowedCSSProperties = [
+  'left','top','width','height',
+  'background','color','fontFamily',
+  'fontSize',
+ ];
+ const allowedHTMLProperties = [
+  'innerHTML','classList'
+ ];
 	let remove = false;
 
 	document.querySelector('#add-cell').onclick = () => {
 		if(remove){
 			remove = false;
-			document.querySelector('#rem-cell').style.backgroundColor = '#6c6';
+			document.querySelector('#rem-cell').style.backgroundColor = getComputedStyle(document.getElementsByTagName('body')[0]).getPropertyValue('--primary');
 		}
 		else{
-			const cell = document.createElement('div');	
+   const c = document.getElementById('widget');
+   let cell;
+   if(c.options[c.selectedIndex].value === 'textarea'){
+    cell = document.createElement('textarea');	
+   }else{
+    cell = document.createElement('div');
+   }
 			cell.style.position = 'absolute';
 			cell.style.left =       document.querySelector('#x').value;
 			cell.style.top =        document.querySelector('#y').value;
 			cell.style.width =      document.querySelector('#width').value;
 			cell.style.height =     document.querySelector('#height').value;
 			cell.style.background = document.querySelector('#background').value;
+   cell.style.color =      document.querySelector('#color').value;
+   if(typeof document.querySelector('#font').value !== 'undefined' && document.querySelector('#font').value.length > 0){
+    WebFont.load({
+     'google':{
+      'families':[document.querySelector('#font').value]
+     }
+    });
+   }
+   cell.style.fontFamily = document.querySelector('#font').value;
+   cell.style.fontSize =   document.querySelector('#fontsize').value;
+   cell.innerHTML =        document.querySelector('#text').value;
 			cell.classList.add('dashb-element');
 			cell.onclick = () => {
 				if(remove){
 					document.body.removeChild(cell);
 					remove = false;
-					document.querySelector('#rem-cell').style.backgroundColor = '#6c6';
+					document.querySelector('#rem-cell').style.backgroundColor = getComputedStyle(document.getElementsByTagName('body')[0]).getPropertyValue('--primary');
 				}
 			}
+   if(c.options[c.selectedIndex].value === 'clock') {
+    cell.classList.add('clock');
+    cell.innerHTML = new Date().toLocaleTimeString();
+    setInterval(() => {
+     cell.innerHTML = new Date().toLocaleTimeString();
+    },1000);
+   }
 			document.body.appendChild(cell);
 		}
 	};
 
 	document.querySelector('#rem-cell').onclick = () => {
 		remove = true;
-		document.querySelector('#rem-cell').style.backgroundColor = '#c66';
+		document.querySelector('#rem-cell').style.backgroundColor = getComputedStyle(document.getElementsByTagName('body')[0]).getPropertyValue('--tertiary');
 	}
 
 	document.querySelector('#download').onclick = () => {
 		const profile = {'cells':[]};
 		[].forEach.call(document.getElementsByClassName('dashb-element'),v => {
-			profile.cells.push({'x':v.style.left,'y':v.style.top,'width':v.style.width,
-                       'height':v.style.height,'background':v.style.backgroundColor});
+   let cell = {},prop;
+   for(let i=0;i<allowedCSSProperties.length;i++){
+    prop = allowedCSSProperties[i];
+   }
+			profile.cells.push();
 		});
 		const a = document.createElement('a');
 		a.setAttribute('href','data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(profile)));
@@ -45,11 +80,4 @@ window.onload = () => {
 		a.click();
 		document.body.removeChild(a);
 	};
-
-	[...document.getElementsByClassName('clock')].forEach(v => {
-		v.innerHTML = new Date().toLocaleTimeString();
-		setInterval(() => {
-			v.innerHTML = new Date().toLocaleTimeString();
-		},1000);
-	});
-}
+};
